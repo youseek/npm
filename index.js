bc71c17a3de02298f5670794468217905c26bb38
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-28 15:16:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const sum = function (a, b) {
+  return a + b
+}
+sum(1, 2)
+const minus = function (a, b) {
+  return a - b
+}
+minus(1, 2)
+const multiply = function (a, b) {
+  return a * b
+}
+multiply(1, 2)
+
+const divide = function (a, b) {
+  return a / b
+}
+divide(1, 2)
